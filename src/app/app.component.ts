cd0a944b48c0c6401c7e63e77950e1938be1003e
emmaengl <emma@englishby.co>
2022-09-25 17:26:56 +0200

    Imports JSON links and logs to console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import Links from '../files/links.json'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'englishby-angular';
+  constructor() {
+    console.log(Links);
+  }
 }
